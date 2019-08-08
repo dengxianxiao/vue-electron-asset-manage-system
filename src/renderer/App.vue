b8fe23594b2404263b2a8d649b4a162f37e8fcb5
dengxianxiao <dengxianxiao@gmail.com>
2019-08-08 10:55:45 +0800

    初始化项目

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue-electron-asset-manage-system'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
